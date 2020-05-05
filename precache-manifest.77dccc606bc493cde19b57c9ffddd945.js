6cdbb3691c313b37c36e7afe99d56a6db26c98ee
Yegor Ivanovskyy <joy_tailor@Yegor.computer>
2020-05-05 18:14:02 +0300

    Updates

---
+++ precache-manifest.77dccc606bc493cde19b57c9ffddd945.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3903c20e1b0ccd2bf7c33a75598227b0",
+    "url": "/clicker-game/index.html"
+  },
+  {
+    "revision": "9a5fb293fdb902e47504",
+    "url": "/clicker-game/static/css/main.acf9a7f7.chunk.css"
+  },
+  {
+    "revision": "c5664e7fffe855e00f8d",
+    "url": "/clicker-game/static/js/2.78b9a5a0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/clicker-game/static/js/2.78b9a5a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a5fb293fdb902e47504",
+    "url": "/clicker-game/static/js/main.1d4d112a.chunk.js"
+  },
+  {
+    "revision": "180ac5edaf2409688a81",
+    "url": "/clicker-game/static/js/runtime-main.1f0759d1.js"
+  }
+]);
